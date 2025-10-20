241262008249bd92a8ef8527f74786f1fd09e9ac
taiwaneve <steve77810@gmail.com>
2025-10-20 16:06:55 +0800

    2

--- app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.Arrangement // 新增
 import androidx.compose.foundation.layout.Column // 新增
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -29,6 +30,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment // 新增
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color // 新增
@@ -67,11 +73,13 @@ fun MainContent(modifier: Modifier = Modifier) {
         R.drawable.animal6, R.drawable.animal7,
         R.drawable.animal8, R.drawable.animal9
     )
+    var mper: MediaPlayer? by remember { mutableStateOf(null) }
+    LocalContext.current
 
     // 修復 AnimalsName 缺少的部分
     val AnimalsName: List<String> = listOf(
         "鴨子1.", "企鵝2.",
-        "青蛙3.", "貓頭鷹4.", "海豚5.", "牛6.", "無尾熊7.", "獅子8.", "狐狸9.", "小雞10"
+        "青蛙3.", "貓頭鷹4.", "海豚5.", "牛6.", "無尾熊7.", "獅子8.", "狐狸9.", "小雞10."
     )
 
     val context = LocalContext.current
@@ -146,21 +154,62 @@ fun MainContent(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.size(10.dp))
         Row{
             Button(onClick = {
-            }) {
-                Text(text = "歡迎修課")
+                mper?.release()  //釋放資源
+                mper = null // 清除舊引用
+                mper = MediaPlayer.create(context, R.raw.tcyang) //設定音樂
+                mper?.start()  } , //開始播放
+                modifier = Modifier
+                    .fillMaxWidth(0.33f)
+                    .fillMaxHeight(0.8f),
+                colors = buttonColors(Color.Green)
+            ) {
+                Text(text = "歡迎", color = Color.Blue)
+                Text(text = "修課", color = Color.Red)
+                Image(
+                    painterResource(id = R.drawable.teacher),
+                    contentDescription ="teacher icon")
             }
 
             Spacer(modifier = Modifier.size(10.dp))
 
-            Button(onClick = {
-            }) {
-                Text(text = "展翅飛翔")
+            Button(
+                onClick = {
+                    mper?.release()  //釋放資源
+                    mper = null // 清除舊引用
+                    mper = MediaPlayer.create(context, R.raw.fly) //設定音樂
+                    mper?.start()  },  //開始播放
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .fillMaxHeight(0.4f),
+                colors = buttonColors(Color.Blue)
+            ) {
+                Text(text = "展翅飛翔", color = Color.White)
+                Image(
+                    painterResource(id = R.drawable.fly),
+                    contentDescription ="fly icon")
             }
+
 
             Spacer(modifier = Modifier.size(10.dp))
 
             Button(onClick = {
-            }) {
+                // 將 Context 安全地轉換(as?)為 Activity，並儲存到變數中
+                val activity = context as? Activity
+                // 如果成功轉換為 Activity，則呼叫 finish()
+                activity?.finish() },
+
+                // 設定按鈕顏色為亮藍色
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF00BFFF)),
+
+                //形狀：將元素的每個角落「切掉」一個直角
+                shape = CutCornerShape(10),
+
+                //藍色框線
+                border = BorderStroke(1.dp, Color.Blue),
+
+                //陰影
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 10.dp)
+        ) {
                 Text(text = "結束App")
             }
         }
